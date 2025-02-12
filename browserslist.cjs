b2e2b64a64f0e31584e210804909713f0dc368af
Tobias Høegh <tobias@tujo.no>
2025-02-12 21:24:56 +0100

    feat: add basis config

--- browserslist.cjs
+++ browserslist.cjs
@@ -1,1 +1,10 @@
-module.exports = []
+module.exports = [
+  'chrome >= 109',
+  'firefox >= 115',
+  'edge >= 109',
+  'opera >= 95',
+  'safari >= 13.1',
+  'ChromeAndroid >= 106',
+  'samsung >= 17',
+  'iOS >= 13.1',
+]
